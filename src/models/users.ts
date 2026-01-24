965d985704664b7eca3684f511847f4c861db143
Yimy.Romero <yimy.romero80@gmail.com>
2026-01-23 22:04:35 -0500

    refactor: update and add new column to the users model.

--- src/models/users.ts
+++ src/models/users.ts
@@ -4,13 +4,15 @@ import {
 	integer,
 	text,
 	timestamp,
+	boolean,
 } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }),
 	email: varchar({ length: 255 }).notNull().unique(),
 	password_hash: text().notNull(),
-	name: varchar({ length: 255 }),
-	role: varchar({ length: 50 }).default("user"),
+	roles: varchar({ length: 50 }).default("user"),
 	created_at: timestamp().defaultNow(),
+	active: boolean().default(false),
 });
